81216ad52aed74a2e6ca1baa06fb3f7223d9884c
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2024-03-21 15:12:28 -0300

    Setup, Header and Footer

---
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import { usePathname } from "next/navigation";
+import { Footer, Media } from "../../../../payload/payload-types";
+import { inclusions, noHeaderFooterUrls } from "../../../constants";
+
+import classes from './index.module.scss';
+import { Gutter } from "../../Gutter";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "../../Button";
+
+export default function FooterComponent({ footer } : {
+  footer : Footer
+}) {
+  const pathname = usePathname();
+
+  const navItems = footer?.navItems;
+
+  return (
+    <footer className={noHeaderFooterUrls.includes(pathname) ? 
+        classes.hide : ''
+      }
+    >
+      <Gutter>
+        <ul className={classes.inclusions}>
+          { inclusions.map((inclusion, index) => {
+            return (
+              <li key={inclusion.title}>
+                <Image 
+                  src={inclusion.icon}
+                  alt={inclusion.title}
+                  width={36}
+                  height={36}
+                  className={classes.icon}
+                />
+
+                <h5 className={classes.title}>{inclusion.title}</h5>
+                <p>
+                  {inclusion.description}
+                </p>
+              </li>
+            )
+          }) }
+        </ul>
+      </Gutter>
+
+      <div className={classes.footer}>
+        <Gutter>
+          <div className={classes.wrap}>
+            <Link href="/">
+              <Image
+                src="/logo-white.svg"
+                alt="logo"
+                width={170}
+                height={50}
+              />
+            </Link>
+
+            <p>
+              {footer.copyright}
+            </p>
+
+            <div className={classes.socialLinks}>
+              { navItems.map((item) => {
+                const icon = item?.link?.icon as Media;
+
+                return (
+                  <Button 
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    className={classes.socialLinkItem}
+                  >
+                    <Image
+                      src={icon?.url}
+                      alt={icon?.alt}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              }) }
+            </div>
+          </div>
+        </Gutter>
+      </div>
+    </footer>
+  )
+}
