49fa91cdd0af0a1dc8f177bebbffaf3a636fd169
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2024-03-22 00:47:39 -0300

    Removing unnecessary imports

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Metadata } from 'next'
 
-import { Gutter } from '../../_components/Gutter'
 import { RenderParams } from '../../_components/RenderParams'
 import { getMeUser } from '../../_utilities/getMeUser'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
